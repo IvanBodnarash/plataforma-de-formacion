ff9a640ca97ccd090499ea3f43d6819bb40fb4f9
Ivan Bodnarash <bodnarash1999@gmail.com>
2025-07-02 12:33:43 +0200

    Error page adjusting2

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,11 @@ export default function NotFound({ id }) {
   return (
     <div className="d-flex flex-column text-center text-light fs-1">
       <h1>404</h1>
-      <h2>Curso con id: {id} no encontrado</h2>
+      {id ? (
+        <h2>Curso con id: {id} no encontrado</h2>
+      ) : (
+        <h2>Ocurrió un error</h2>
+      )}
     </div>
   );
 }
